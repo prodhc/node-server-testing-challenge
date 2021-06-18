a6036d657fd96e7a12efada93feb953c145f242d
prodhc <zionywood@gmail.com>
2021-06-17 19:29:45 -0700

    set up

---
+++ api/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 7000;
+
+server.listen(PORT, () => {
+	console.log(`\n*** Server Running on http://localhost:${PORT}***\n`);
+});
